72ee44c26b104a0a3cab264aea80afc710f53e42
Your Name <you@example.com>
2020-07-17 17:01:07 +0300

    baseprinciples

--- 1.3_2_call_stack_combination.py
+++ 1.3_2_call_stack_combination.py
@@ -1,4 +1,5 @@
 # function of combination C[n][k]
+# variant 1
 n, k = map(int, input().split())
 
 sz = max(n, k) + 1
@@ -10,3 +11,22 @@ for i in range(1, sz):
         c[i][j] = c[i - 1][j] + c[i - 1][j - 1]
 
 print(c[n][k])
+
+# variant 2
+def f(x):
+	if x == 0:
+		return 1
+	return f(x-1) * x
+
+def C(n,k):
+	if k == 0:
+		return 1
+	elif k > n:
+		return 0
+	r = f(n)/(f(n-k)*f(k))
+	return int(r)
+n, k = map(int, input().split())
+
+
+
+print(C(n,k))
