16018bfca55c1ef7b713684a0fae2eae33d62243
Your Name <you@example.com>
2020-07-18 16:50:45 +0300

    NameSpace&RangeOfVisibility

---
+++ 1.4_NameSpace.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+parent = {"global": None}
+vs = {"global": set()}
+
+for _ in range(n):
+    t, fst, snd = input().split()
+    if t == "create":
+        parent[fst] = snd
+        vs[fst] = set()
+    elif t == "add":
+        vs[fst].add(snd)
+    else:  # t == get
+        while fst is not None:
+            if snd in vs[fst]:
+                break
+            fst = parent[fst]
+        print(fst)
